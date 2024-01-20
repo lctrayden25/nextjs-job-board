a541a2432ac4d42299f74c08cea0fa0cc32ad2e6
Rayden Li <79618253+lctrayden25@users.noreply.github.com>
2024-01-21 01:51:17 +0800

    add params to url when do job filter

--- src/components/JobFilterSidebar.tsx
+++ src/components/JobFilterSidebar.tsx
@@ -4,9 +4,27 @@ import { Input } from "./ui/input";
 import Select from "./ui/select";
 import { jobTypes } from "@/lib/job-types";
 import { Button } from "./ui/button";
+import { jobFilterSchema } from "@/lib/validation";
+import { redirect } from "next/navigation";
 
 async function filterJobs(formData: FormData) {
   "use server";
+
+  const values = Object.fromEntries(formData.entries());
+  const parseResult = jobFilterSchema.parse(values);
+
+  const { q, location, type, remote } = parseResult;
+
+  const searchParams = new URLSearchParams({
+    ...(q && { q: q.trim() }),
+    ...(location && { location }),
+    ...(type && { type }),
+    ...(remote && { remote: "true" }),
+  });
+
+  redirect(`/?${searchParams.toString()}`);
+
+
 }
 
 const JobFilterSidebar = async () => {
