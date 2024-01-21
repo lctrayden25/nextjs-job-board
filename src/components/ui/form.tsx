3ef838e33a4805ca05ea3bd5fcf3e07940b4b2aa
Rayden Li <79618253+lctrayden25@users.noreply.github.com>
2024-01-21 14:58:39 +0800

    remove error color from form field title

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -87,13 +87,13 @@ FormItem.displayName = "FormItem"
 const FormLabel = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => {
+>((props, ref) => {
   const { error, formItemId } = useFormField()
 
   return (
     <Label
       ref={ref}
-      className={cn(error && "text-destructive", className)}
+      // className={props.className}
       htmlFor={formItemId}
       {...props}
     />
