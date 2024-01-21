8e31c12ce70c1aabc270b04813682ac440477c2b
Rayden Li <79618253+lctrayden25@users.noreply.github.com>
2024-01-21 13:30:57 +0800

    add navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+"use client"
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="mx-auto max-w-5xl space-y-5 px-3 py-5">
+        <div className="flex flex-col justify-between gap-3 sm:flex-row sm:items-center">
+          <div className="space-y-2">
+            <h3 className="text-xl font-semibold">Job Board</h3>
+          </div>
+          <div className="flex flex-wrap gap-5 text-sm text-muted-foreground">
+            <Link href="/about" className="hover:underline">
+              About Us
+            </Link>
+            <Link href="/contact" className="hover:underline">
+              Contact
+            </Link>
+            <Link href="/terms" className="hover:underline">
+              Terms of Service
+            </Link>
+            <Link href="/privacy" className="hover:underline">
+              Privacy Policy
+            </Link>
+          </div>
+        </div>
+        {/* <div className="text-center text-sm text-muted-foreground">
+          © {new Date().getFullYear()} Flow Jobs, Inc. All rights reserved.
+        </div> */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
