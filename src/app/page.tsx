0b050456aa5a5912beaa7182e55afeb3f0ba7b7d
Rayden Li <79618253+lctrayden25@users.noreply.github.com>
2024-01-21 13:52:03 +0800

    update site meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import JobResults from "@/components/JobResults";
 import H1 from "@/components/ui/h1";
 import prisma from "@/lib/prisma";
 import { JobFilterValues } from "@/lib/validation";
+import { Metadata } from "next";
 
 interface PageProps {
   searchParams: {
@@ -13,6 +14,33 @@ interface PageProps {
     remote?: string;
   };
 }
+
+const getTitle = ({ q, type, location, remote }: JobFilterValues) => {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+      ? `${type} developer jobs`
+      : remote
+        ? "Remote developer jobs"
+        : "All developer jobs";
+
+  const titleSuffic = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffic}`;
+};
+
+export const generateMedata = ({
+  searchParams: { q, type, location, remote },
+}: PageProps): Metadata => {
+  return {
+    title: `${getTitle({
+      q,
+      type,
+      location,
+      remote: remote === "true",
+    })} | Job Board`,
+  };
+};
 
 export default async function Home({
   searchParams: { q, type, location, remote },
@@ -27,11 +55,11 @@ export default async function Home({
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <H1>Developer Jobs</H1>
+        <H1>{getTitle(filterValues)}</H1>
         <p className="text-muted-foreground">Find your dream job.</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
-        <JobFilterSidebar defaultValues={filterValues}/>
+        <JobFilterSidebar defaultValues={filterValues} />
         <JobResults filterValues={filterValues} />
       </section>
     </main>
